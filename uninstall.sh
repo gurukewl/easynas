3445a01234b850e06776eff1b89c50e2fe311716
Arijit Bhattacharyya <1336718+gurukewl@users.noreply.github.com>
2024-06-22 15:53:28 +0000

    Adding Uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo "=================================================================================="
+echo "Uninstalling EasyNAS..."
+echo "=================================================================================="
+DEBIAN_FRONTEND=noninteractive \apt purge samba rsync php-cgi git mdadm smartmontools cryptsetup apt-transport-https curl gnupg-agent software-properties-common dnsutils rclone -y
+echo "================================================================================="
+echo "Uninstall Docker"
+echo "================================================================================="
+apt-get purge docker-ce
+rm -rf /var/lib/docker 
+apt autoremove
+echo "=================================================================================="
+echo "Removing Samba configuration"
+echo "=================================================================================="
+rm /etc/samba/shares.conf
+rm -rf /etc/samba/shares
+echo "=================================================================================="
+echo "Removing Volumes Directory for Volume mounts"
+echo "=================================================================================="
+rm -rf /volumes
+echo "=================================================================================="
+echo "Removing Filebrowser Service..."
+echo "=================================================================================="
+systemctl stop filebrowser
+systemctl disable --now filebrowser
+rm /etc/systemd/system/filebrowser.service
+echo "=================================================================================="
+echo "Removing easynas Service..."
+echo "=================================================================================="
+systemctl stop easynas
+systemctl disable --now easynas
+rm /etc/systemd/system/easynas.service
+echo "==============================================================================================================================="
+echo "             EasyNAS unistallation complete. Reboot Now.								 	                                 "
+echo "==============================================================================================================================="
