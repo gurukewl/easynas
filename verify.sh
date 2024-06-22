ef283569b59920ad7136e0c1bd045c0d87040b4c
Arijit Bhattacharyya <1336718+gurukewl@users.noreply.github.com>
2024-06-22 15:14:00 +0000

    update verify.sh method --need to add admin check

--- verify.sh
+++ verify.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
 
-id -u $1 > /dev/null
-if [ $? -ne 0 ]
-then
-  echo "0"
-  exit 1
-else
-  export ADMINUSER=`groups "$1" | grep -w admins`
-  if [ -z "$ADMINUSER" ]
-  then
-    echo "0"
-    exit 1
-  fi
-  PASSWD=$2
-  export PASSWD
-  ORIGPASS=`grep -w "$1" /etc/shadow | cut -d: -f2`
-  export ALGO=`echo $ORIGPASS | cut -d'$' -f2`
-  export SALT=`echo $ORIGPASS | cut -d'$' -f3`
-  GENPASS=$(perl -le 'print crypt("$ENV{PASSWD}","\$$ENV{ALGO}\$$ENV{SALT}\$")')
-  
-  if [ "$GENPASS" == "$ORIGPASS" ]
-  then
+# Get the username and password from the command line arguments.
+username="$1"
+password="$2"
+
+# Check if the user exists.
+if id -u "$username" > /dev/null; then
+  # Hash the password using the same algorithm and salt as in the original script.
+  salt=$(grep -w "$username" /etc/shadow | cut -d: -f3)
+  hash=$(perl -le 'print crypt("$ENV{password}","\$$ENV{salt}\$")')
+
+  # Compare the hashed password with the user's password hash in /etc/shadow.
+  if cat /etc/shadow | grep -q "^$username:$hash:"; then
     echo "1"
-    exit 0
   else
     echo "0"
-    exit 1
   fi
+else
+  echo "Error: User not found."
+  exit 1
 fi
